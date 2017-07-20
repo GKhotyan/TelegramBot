20260b2c89e833a58b843956f47411ab632b8659
Alexey <alexey.basov@masterdata.ru>
2017-07-20 11:59:29 +0300

    Fix after merge

--- src/main/java/parsers/ChampionatParser.java
+++ src/main/java/parsers/ChampionatParser.java
@@ -2,8 +2,6 @@ package parsers;
 
 import data.ChampionatNewsData;
 import data.serialize.ChampionatNewsSerializer;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,11 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import populators.RfplNewsPopulator;
 import utils.TelegramSender;
-import utils.net.WebClient;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Optional;
 
 @Component
 public class ChampionatParser {
@@ -28,11 +24,8 @@ public class ChampionatParser {
   @Autowired
   RfplNewsPopulator rfplNewsPopulator;
   @Autowired
-  private WebClient webClient;
-  @Autowired
   TelegramSender telegramSender;
 
-  private final static String SCORE_URL = "https://www.championat.com/live/live.json";
   private static final String url = "https://www.championat.com/football/_russiapl.html";
 
   public void parseNews(){
@@ -66,43 +59,9 @@ public class ChampionatParser {
     }
   }
 
-    public String getScores() {
-        StringBuilder result = new StringBuilder();
-        try {
-            Optional<JSONObject> json = webClient.httpGet(SCORE_URL);
-            json.ifPresent(j -> {
-                JSONObject football = (JSONObject) j.get("football");
-                JSONObject match_data = (JSONObject) football.get("match_data");
-                for (Object key : match_data.keySet()) {
-                    JSONObject champ = (JSONObject) match_data.get(key);
-                    String title = champ.get("name") + "\n---------------\n";
-                    JSONArray data = (JSONArray) champ.get("m_data");
-                    StringBuilder results = new StringBuilder();
-                    for (Object d : data) {
-                        JSONObject match = (JSONObject) d;
-                        if (match.get("on_main").equals("1")) {
-                            results.append(match.get("time")).append(" ").append(match.get("name")).append(" ").append(match.get("result")).append(" (").append(match.get("status")).append(")\n");
-                        }
-                    }
-                    if (results.length() != 0) {
-                        result.append(title).append(results);
-                    }
-                    result.append("\n");
-                }
-            });
-        }
-        catch (Throwable th) {
-            return "Все пошло по пизде.";
-        }
-        if(result.length() == 0) {
-            return "Бот не смог ничего найти. Виноват - Виталик.";
-        }
-        return rfplNewsPopulator.populate(result.toString());
-    }
-
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         ChampionatParser championatParser = new ChampionatParser();
-        championatParser.getScores();
+        championatParser.parseNews();
 
   }
 }
