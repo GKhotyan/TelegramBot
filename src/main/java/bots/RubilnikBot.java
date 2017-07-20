2e9beb3f31c21b72dcf926ccec54b3c6e03b2595
GKhotyan <georgiy.hotiyan@masterdata.ru>
2017-07-20 14:06:55 +0300

    get Author changed

--- src/main/java/bots/RubilnikBot.java
+++ src/main/java/bots/RubilnikBot.java
@@ -41,8 +41,8 @@ public class RubilnikBot extends TelegramLongPollingBot {
   String swearPatterns = "бот";
 
   private String getContact(Message message) {
-    if(message!=null && message.getFrom()!=null && message.getFrom().getUserName()!=null) {
-      return message.getFrom().getUserName();
+    if(message!=null && message.getFrom()!=null) {
+      return message.getFrom().getUserName()==null?message.getFrom().getFirstName():message.getFrom().getUserName();
     }
     return null;
   }
